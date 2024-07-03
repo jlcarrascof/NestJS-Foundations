5b11b528f19c02c57094ddf1a0a5fe4302d57615
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-03 10:26:28 -0400

    chore: Add DiaristaModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DiaristaModule } from './diarista/diarista.module';
 
 @Module({
-  imports: [],
+  imports: [DiaristaModule],
   controllers: [],
   providers: []
 })
