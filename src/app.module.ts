6e950611f7e47ecf142a0458fdd6ea96a8bc7a80
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-30 23:13:00 -0400

    feat: Add TypeORM configuration for MySQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DiaristaModule } from './diarista/diarista.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DiaristaModule],
+  imports: [DiaristaModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'curso_nest',
+      entities: [],
+      synchronize: true,
+  })],
   controllers: [],
   providers: []
 })
